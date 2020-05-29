b289458d3813f45173dc125814f68920088ca6f3
dinalusiana <53215221+dinalusiana@users.noreply.github.com>
2020-05-29 23:17:18 +0700

    Add files via upload

---
+++ formulir.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 29 Bulan Mei 2020 pada 18.00
+-- Versi server: 10.1.30-MariaDB
+-- Versi PHP: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `formulir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `form`
+--
+
+CREATE TABLE `form` (
+  `ID` int(11) NOT NULL,
+  `nama_lengkap` varchar(20) DEFAULT NULL,
+  `jenis_kelamin` varchar(20) DEFAULT NULL,
+  `nama_sekolah` varchar(20) DEFAULT NULL,
+  `telp` varchar(20) DEFAULT NULL,
+  `survei1` varchar(50) DEFAULT NULL,
+  `survei2` varchar(50) DEFAULT NULL,
+  `survei3` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `form`
+--
+
+INSERT INTO `form` (`ID`, `nama_lengkap`, `jenis_kelamin`, `nama_sekolah`, `telp`, `survei1`, `survei2`, `survei3`) VALUES
+(3, 'Dina', 'Perempuan', 'SMAN 1', '08743635', 'good', 'job', 'yaa');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `form`
+--
+ALTER TABLE `form`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `form`
+--
+ALTER TABLE `form`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
